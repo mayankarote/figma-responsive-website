97ea25280a6fca1588ed24cfd4bda27b816ddf31
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-07-21 11:08:41 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -57,7 +57,7 @@ function App() {
               <Login />
             </Route>
 
-            <Route path="/">
+            <Route path="/login">
               <Home />
             </Route>
           </Switch>
