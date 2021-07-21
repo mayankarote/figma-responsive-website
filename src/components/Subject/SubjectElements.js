e0778e45541b369a174ea493b30a6c0085fd57c9
Mayanka-88 <70584944+Mayanka-88@users.noreply.github.com>
2021-07-21 13:48:18 +0530

    Updated

--- src/components/Subject/SubjectElements.js
+++ src/components/Subject/SubjectElements.js
@@ -5,7 +5,6 @@ export const SubjectContainer = styled.div`
 `;
 export const SubjectContent = styled.div`
   max-height: 100%;
-  height: calc(100vh + 250px);
   background: #0c2651;
   padding: 0rem calc((100vw - 1500px) / 2);
   @media screen and (max-width: 650px) {
